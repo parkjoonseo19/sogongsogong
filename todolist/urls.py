f4cfb1a4f393d1b5e8914979fe541426a914bcdb
hijjoy <hijjoy@naver.com>
2023-06-09 01:53:46 +0900

    feat: 💄 work-management templates 추가

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from todolist import views
 
 urlpatterns = [
-    path('',views.ListAndWork, name='ListAndWork')
+    path('',views.ListAndWork, name='ListAndWork'),
+    path('workMangement/', views.test, name='test')
 ]
