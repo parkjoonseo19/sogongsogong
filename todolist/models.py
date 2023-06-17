ce4025139e1981e6228c152f4c0bebe69cca3ab5
hijjoy <hijjoy@naver.com>
2023-06-18 01:12:26 +0900

    fix : 🚑 workData&listData django-rest-api 코드로 변경

--- todolist/models.py
+++ todolist/models.py
@@ -11,10 +11,10 @@ class ListData(models.Model) :
 class WorkData(models.Model) :
     workName = models.CharField(max_length=100)
     workPriority =  models.IntegerField(null=True,blank=True)
-    workDeadline = models.DateTimeField(null=True,blank=True)
+    workDeadline = models.DateField(null=True,blank=True)
     completed = models.BooleanField(default=False)
     
-    workList = models.ForeignKey(ListData, on_delete=models.CASCADE)
+    workList = models.ForeignKey(ListData, related_name='work_data', on_delete=models.CASCADE)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
