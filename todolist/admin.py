c1b58ae973242cf447d35077688cfe386cea3f40
hijjoy <hijjoy@naver.com>
2023-06-18 09:40:04 +0900

    docs : 🔥 remove files

--- todolist/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ListData,WorkData
-
-admin.site.register(ListData)
-admin.site.register(WorkData)
--- todolist/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-    path('work-list/workData/<int:pk>',WorkDataDetail.as_view())
