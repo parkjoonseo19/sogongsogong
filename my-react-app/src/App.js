6ac6650ca0bfc83007c1a4d1c180ffa3a55a90aa
hijjoy <hijjoy@naver.com>
2023-06-21 02:58:36 +0900

    fix : 🚑 화면 중첩 all 수정 & 목록에 대한 작업 뜨는거 수정

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,207 +7,305 @@ import Edit from "./Edit";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Card({ id, title, content, onDelete, todos }) {
+  const [cards, setCards] = useState([{ id: 0, todos: [] }]); // 초기에 기본 카드 하나를 추가
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const parsedContent = JSON.parse(content);
+    setTasks(parsedContent);
+  }, [content]);
+
   const [defaultListText, setDefaultListText] = useState([]);
+
+  useEffect(() => {
+    setDefaultListText([title]);
+  }, [title]);
   const [isEditingDefaultList, setIsEditingDefaultList] = useState(false);
-  const [sortChange, setSortChange] = useState(false);
+  const [sortChange, setSortChange] = useState(false); //정렬버튼 누를때마다 데드라인 정렬부터 번갈아가면서 정렬하기 위해 만든 state
   const [sortImage, setSortImage] = useState("/image/arrange.png");
 
-  const handleDefaultListTextChange = (event) => {
-    setDefaultListText(event.target.value);
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         const token = localStorage.getItem("token");
+  //
+  //         const response = await fetch(
+  //           "http://localhost:8000/todolist/work-list/",
+  //           {
+  //             headers: {
+  //               Authorization: `Token ${token}`,
+  //             },
+  //           }
+  //         );
+  //         if (!response.ok) {
+  //           throw new Error("Network response was not ok");
+  //         }
+  //         const data = await response.json();
+  //
+  //         setTodos(data);
+  //       } catch (error) {
+  //         console.error("Error fetching data: ", error);
+  //       }
+  //     };
+  //
+  //     fetchData();
+  //   }, []);
+  const handleCheckboxChange = (taskPK) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.pk === taskPK ? { ...task, completed: !task.completed } : task
+      )
+    );
   };
 
-  const handleAddTask = () => {
-    const year = 2023;
-    const month = "06";
-    const day = String(Math.floor(Math.random() * 30) + 1).padStart(2, "0");
-    const deadline = `${year}-${month}-${day}`;
+  // 중복되지 않는 랜덤 수 생성 함수(테스트용)
+  function generateUniqueRandomNumber() {
+    const usedNumbers = tasks.map((task) => parseInt(task.priority));
+    let randomNumber = getRandomNumber();
 
-    const priority = generateUniqueRandomNumber();
+    while (usedNumbers.includes(randomNumber)) {
+      randomNumber = getRandomNumber();
+    }
 
-    const newTask = {
-      id: todos.length,
-      text: `작업 ${todos.length + 1}`,
-      checked: false,
-      deadline: deadline,
-      priority: priority.toString(),
-    };
+    return randomNumber;
+  }
+  // 랜덤 수 생성 함수(테스트용)
+  function getRandomNumber() {
+    // 원하는 수의 범위를 지정하세요 (예: 1부터 10까지)
+    const min = 1;
+    const max = 10;
 
-    todos.push(newTask); // 카드에 새로운 테스크 추가
-
-    setDefaultListText([...defaultListText]); // 상태 업데이트
-  };
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   const handleSortTasks = () => {
-    // 정렬 로직
-    todos.sort((a, b) => {
+    //정렬
+    setTasks((prevTasks) => {
       if (sortChange) {
-        return Number(a.priority) - Number(b.priority);
+        return [...prevTasks].sort(
+          (a, b) => Number(a.priority) - Number(b.priority)
+        ); //데드라인 정렬
       } else {
-        return a.deadline > b.deadline ? 1 : -1;
+        return [...prevTasks].sort((a, b) =>
+          a.deadline > b.deadline ? 1 : -1
+        ); //우선순위 정렬
       }
     });
 
-    setSortChange(!sortChange); // 정렬 상태 업데이트
-
+    setSortChange((prevValue) => !prevValue);
     setSortImage((prevImage) => {
       return prevImage === "/image/arrange.png"
         ? "/image/darrange.png"
         : "/image/arrange.png";
     });
   };
-
   const handleDelete = () => {
     onDelete(id);
   };
 
+  const handleDefaultListTextChange = (event) => {
+    setDefaultListText(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    const year = 2023; //이거는 테스트용으로 날짜랜덤생성한거. 작업수정페이지랑 연결하면 삭제
+    const month = "06";
+    const day = String(Math.floor(Math.random() * 30) + 1).padStart(2, "0");
+    const deadline = `${year}-${month}-${day}`; //여기까지 날짜랜덤생성
+
+    const priority = generateUniqueRandomNumber(); //우선순위 랜덤 생성
+
+    const newTask = {
+      id: tasks.length,
+      text: `작업 ${tasks.length + 1}`,
+      checked: false,
+      deadline: deadline,
+      priority: priority.toString(),
+    };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    console.log("Deadline:", deadline);
+    console.log("Priority:", priority);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
+  const addCard = () => {
+    const newCard = { id: Date.now() }; // 새로운 카드의 id를 현재 시간으로 생성
+    setCards((prevCards) => [...prevCards, newCard]);
+  };
+
   return (
-    <div className="card">
-      <ul className="task-list">
-        <div id="work_head">
-          <div class="ardelbtn">
-            <button
-              id="workname"
-              onClick={() => setIsEditingDefaultList(true)}
-            >
-              {isEditingDefaultList ? (
-                <input
-                  type="text"
-                  value={defaultListText}
-                  onChange={handleDefaultListTextChange}
-                  onBlur={() => setIsEditingDefaultList(false)}
-                />
-              ) : (
-                defaultListText
-              )}
-            </button>
-          </div>
-          <div class="ardelbtn">
-            <button class="arrangebtn" onClick={handleSortTasks}>
-              <img src={sortImage} alt="정렬" />
-            </button>
-          </div>
-          <div class="ardelbtn">
-            <button class="deletebtn" onClick={handleDelete}>
-              <img src="/image/delete.png" alt="삭제" />
-            </button>
-          </div>
-        </div>
-        <h2>{content}</h2>
-        <div id="checkbox_div">
-          {todos
-            .filter((task) => !task.completed)
-            .map((task) => (
-              <li key={task.pk}>
-                <label className="checkbox-label">
-                  {task.completed ? (
-                    <img
-                      className="checkbox"
-                      src="/image/checkbox_checked.png"
-                      alt="체크박스"
-                    />
-                  ) : (
-                    <img
-                      className="checkbox"
-                      src="/image/checkbox_unchecked.png"
-                      alt="체크박스"
-                    />
-                  )}
-                  <input
-                    type="checkbox"
-                    checked={task.completed}
-                    onChange={() => handleToggle(task)}
-                  />
-                  {task.text}
-                </label>
-              </li>
-            ))}
-        </div>
-        <div id="add_task_div">
-          <button id="add_task_btn" onClick={handleAddTask}>
-            <img src="/image/add_task.png" alt="작업 추가" />
+    <div className="app">
+      <div className="header">
+        <a>
+          <img id="logo" src="/image/logo.png" alt="로고" />
+        </a>
+
+        <div id="user">
+          <div id="user-info">admin 님</div>
+          <button id="logout" onClick={handleLogout}>
+            <img src="/image/loggout.png" alt="로그아웃" />
           </button>
         </div>
-      </ul>
+      </div>
+      <div className="card">
+        <ul className="task-list">
+          <div id="work_head">
+            <div class="ardelbtn">
+              <button
+                id="workname"
+                onClick={() => setIsEditingDefaultList(true)}
+              >
+                {isEditingDefaultList ? (
+                  <input
+                    type="text"
+                    value={defaultListText}
+                    onChange={handleDefaultListTextChange}
+                    onBlur={() => setIsEditingDefaultList(false)}
+                  />
+                ) : (
+                  defaultListText
+                )}
+              </button>
+            </div>
+            <div class="ardelbtn">
+              <button class="arrangebtn" onClick={handleSortTasks}>
+                <img src={sortImage} alt="정렬" />
+              </button>
+            </div>
+            <div class="ardelbtn">
+              <button class="deletebtn" onClick={handleDelete}>
+                <img src="/image/delete.png" alt="삭제" />
+              </button>
+            </div>
+          </div>
+
+          <div id="checkbox_div">
+            {tasks
+              .filter((task) => !task.completed) // 완료 안된 작업
+              .map((task) => (
+                <li key={task.pk}>
+                  <label className="checkbox-label">
+                    {task.completed ? (
+                      <img
+                        src="/image/notfull.png"
+                        alt="체크된 이미지"
+                        onClick={() => handleCheckboxChange(task.pk)}
+                      />
+                    ) : (
+                      <img
+                        src="/image/full2.png"
+                        alt="체크박스 체크안됨"
+                        onClick={() => handleCheckboxChange(task.pk)}
+                      />
+                    )}
+
+                    <span class="checkbox-text">
+                      {" "}
+                      <Link to={`/edit/${task.pk}`}>{task.workName}</Link>
+                    </span>
+                  </label>
+                </li>
+              ))}
+
+            {tasks
+              .filter((task) => task.completed) // 체크된 작업만 필터링
+              .map((task) => (
+                <li key={task.id}>
+                  <label className="checkbox-label_checked">
+                    {task.completed ? (
+                      <img
+                        src="/image/notfull.png"
+                        alt="체크된 이미지"
+                        onClick={() => handleCheckboxChange(task.pk)}
+                      />
+                    ) : (
+                      <img
+                        src="/image/full2.png"
+                        alt="체크박스 체크안됨"
+                        onClick={() => handleCheckboxChange(task.pk)}
+                      />
+                    )}
+                    <span className="checkbox-text_checked">
+                      <Link to={`/edit/${task.pk}`}>{task.workName}</Link>
+                    </span>
+                  </label>
+                </li>
+              ))}
+
+            <button id="workplus" onClick={handleAddTask}>
+              <img src="/image/workplus.png" alt="작업추가" />
+            </button>
+          </div>
+        </ul>
+      </div>
+      <div id="listplus">
+        <button onClick={addCard}>
+          <img src="/image/listplus.png" alt="목록추가" />
+        </button>
+      </div>
     </div>
   );
 }
 
-function App() {
-  const [cards, setCards] = useState([]);
-
+export default function App() {
+  const [cards, setCards] = useState([{ id: 0, todos: [] }]); // 초기에 기본 카드 하나를 추가
+  const [todos, setTodos] = useState([]);
   useEffect(() => {
-    // 초기 카드 설정
-    const initialCards = [
-      {
-        id: 0,
-        title: "카드 1",
-        content: "카드 1 내용",
-        todos: [],
-      },
-      {
-        id: 1,
-        title: "카드 2",
-        content: "카드 2 내용",
-        todos: [],
-      },
-    ];
-    setCards(initialCards);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/todolist/work-list/"
+        );
+        const data = await response.json();
+        setTodos(data);
+        // console.log(data.work_data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const addCard = () => {
-    const newCard = {
-      id: cards.length,
-      title: `카드 ${cards.length + 1}`,
-      content: `카드 ${cards.length + 1} 내용`,
-      todos: [], // 새로운 카드에 빈 테스크 목록 설정
-    };
-
-    setCards([...cards, newCard]);
+    const newCard = { id: Date.now() }; // 새로운 카드의 id를 현재 시간으로 생성
+    setCards((prevCards) => [...prevCards, newCard]);
   };
 
-  const deleteCard = (cardId) => {
-    const updatedCards = cards.filter((card) => card.id !== cardId);
-    setCards(updatedCards);
+  const deleteCard = (id) => {
+    setCards((prevCards) => prevCards.filter((card) => card.id !== id));
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
   };
 
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/edit">Edit</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/edit" element={<Edit />} />
-        </Routes>
-
-        <button onClick={addCard}>카드 추가</button>
-
-        {cards.map((card) => (
-          <Card
-            key={card.id}
-            id={card.id}
-            title={card.title}
-            content={card.content}
-            todos={card.todos} // 각 카드에 해당하는 테스크 목록 전달
-            onDelete={deleteCard}
-          />
-        ))}
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/edit/:pk" element={<Edit todos={todos} />} />
+        <Route
+          path="/"
+          element={todos.map((list) => (
+            <Card
+              key={list.pk}
+              id={list.pk}
+              title={list.listname}
+              todos={todos}
+              content={JSON.stringify(list.work_data)}
+              onDelete={deleteCard}
+            />
+          ))}
+        />
+      </Routes>
     </Router>
   );
 }
-
-export default App;
